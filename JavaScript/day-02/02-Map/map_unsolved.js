8231719f99bf83e2511b93b15c31fdd4fc0e83bb
jimmytu <jimmytutron@gmail.com>
2018-10-23 19:44:52 -0700

    Adding solved and unsolved for .map() code drills

--- JavaScript/day-02/02-Map/map_unsolved.js
+++ JavaScript/day-02/02-Map/map_unsolved.js
@@ -51,40 +51,14 @@ var gummyBearFlavors = [{
 // all functions in this code drill require the use of the .map() method as well as => fat arrow syntax
 
 // create a function that console logs "my favorite gummy bear flavor is <flavor>" for each item in the array.
-var favoriteFlavors = gummyBearFlavors.map((flavor) => {
-    console.log(`my favorite flavor is ${flavor.flavor}`);
-});
+
 
 // create a function that returns a string that looks like "<number>. <flavor> gummy bears are <color>." for each item in the array.
 
-var mapArrayWithIndex = gummyBearFlavors.map(function (flavor, index) {
-    return `${index}. ${flavor.flavor} flavored gummy bears are ${flavor.color}`;
-});
-
-console.log(mapArrayWithIndex);
 
 // create a function that checks whether an item in the array has flavors that are either red or yellow and pushes them to new arrays called redFlavor and yellowFlavor.
 
 var redFlavor = [];
 var yellowFlavor = [];
 
-var checkFlavorColors = gummyBearFlavors.map((flavor) => {
-
-    if (flavor.color === "red") {
-        redFlavor.push(flavor)
-    }
-    if (flavor.color === "yellow") {
-        yellowFlavor.push(flavor)
-    }
-})
-
 // using the new arrays, create a function that console logs "<flavor name> flavored gummy bears are <color>. mmm delicious!"
-
-var logFlavorColors = (flavors) => {
-    flavors.map((flavor) => {
-        console.log(`${flavor.flavor} flavored gummy bears are ${flavor.color}. mmm delicious!`);
-    })
-}
-
-logFlavorColors(redFlavor);
-logFlavorColors(yellowFlavor);
