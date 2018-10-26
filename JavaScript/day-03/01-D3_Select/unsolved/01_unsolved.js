55215daa018d774a17c5a36aa33f04519a812005
jimmytu <jimmytutron@gmail.com>
2018-10-25 17:47:19 -0700

    Add unsolved files

---
+++ JavaScript/day-03/01-D3_Select/unsolved/01_unsolved.js
@@ -0,0 +1,68 @@
+var gabumon = {
+    digivolutions: [
+      {
+        stage: 1,
+        name: "Garurumon"
+      },
+      {
+        stage: 2,
+        name: "WereGarurumon"
+      },
+      {
+          stage: 3,
+          name: "MetalGarurumon"
+      },
+      {
+          stage: 4,
+          name: "ZeedGarurumon"
+      }
+    ],
+    image: "https://vignette.wikia.nocookie.net/digimon/images/d/d1/Gabumon_b.jpg/revision/latest?cb=20171127121438"
+  };
+
+
+// Select the div with a class of tagline
+
+// console.log the text within the tagline
+
+// replace the text with "D3gimon are the Champions!"
+
+
+// Capture the HTML link for "D3 D3 D3givolve"
+
+
+// Select the child element for the link
+
+
+// Capture the child element's href attribute
+
+
+// Change the link to any digimon related video
+
+// using the same selection, change the link's text to and href to the digimon wikipedia page
+
+
+// Create a new <li> element and and append it to the <ul>, give this new element text of "Victory Greymon"
+
+
+// Use chaining to create another new element and set its text as "Omnimon"
+
+
+// Select the div with an id of "agumon" and append a new <img> element using this image "https://vignette.wikia.nocookie.net/digimon/images/6/68/Agumon_b.jpg/revision/latest?cb=20170210150934"
+
+
+// select the div with an id of gabumon
+
+
+// append a new <img> element to the gabumon div using this image https://vignette.wikia.nocookie.net/digimon/images/d/d1/Gabumon_b.jpg/revision/latest?cb=20171127121438
+
+
+// append a new <ul> element to the div
+
+// Using the gabumon object, append <li> elements to the new <ul>. There should be a new list item for each digivolution name
+
+// Select all list items, then change their font color
+
+
+
+  
