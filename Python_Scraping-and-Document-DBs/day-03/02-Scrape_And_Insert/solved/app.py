ff34c2e37279aada6eafb7baa534a403cc01a429
Okens <Ng.Eric314@gmail.com>
2018-11-07 21:19:58 -0800

    Creating initial flask app with home route, a scrape route, and index.html template.

---
+++ Python_Scraping-and-Document-DBs/day-03/02-Scrape_And_Insert/solved/app.py
@@ -0,0 +1,30 @@
+# import necessary libraries
+from flask import Flask, render_template, redirect
+from flask_pymongo import PyMongo
+# import scrape_wiki.py
+
+# create instance of Flask app
+app = Flask(__name__)
+
+# Use flask_pymongo to set up mongo connection
+app.config["MONGO_URI"] = "mongodb://localhost:27017/state_app"
+mongo = PyMongo(app)
+
+@app.route("/")
+def home():
+  # Temporary info for states_info
+  states_info = [
+    {
+    "state":"California",
+    "capital":"Sacramento"
+    }
+  ]
+
+  return render_template("index.html", states_info = states_info)
+
+@app.route("/scrape")
+def scrape():
+  return redirect("/", code=302)
+
+if __name__ == "__main__":
+  app.run(debug=True)
