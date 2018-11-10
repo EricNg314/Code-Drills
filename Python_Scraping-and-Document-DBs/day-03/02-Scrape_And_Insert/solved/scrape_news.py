fd8ebe7e742565f90f45c14fd40c98bc1b6da174
Okens <ng.eric314@gmail.com>
2018-11-10 11:06:44 -0800

    Updating with solved and instructions.

--- Python_Scraping-and-Document-DBs/day-03/02-Scrape_And_Insert/solved/scrape_news.py
+++ Python_Scraping-and-Document-DBs/day-03/02-Scrape_And_Insert/solved/scrape_news.py
@@ -1,6 +1,10 @@
+# import necessary libraries:
+  # Browser from splinter
+  # BeautifulSoup from bs4
 from splinter import Browser
 from bs4 import BeautifulSoup
 
+# ---------------------------------------------------------
 # Define a function called "init_browser" to initialize your browser
 def init_browser():
   # Create a Browser instance from splinter using chrome.
@@ -10,21 +14,28 @@ def init_browser():
   executable_path = {"executable_path": "chromedriver"}
   return Browser("chrome", **executable_path, headless=False)
 
+# ---------------------------------------------------------
 # Function to scrape for a state from nytimes
 def scrape_states():
   # Initialize browser by calling on your init_browser function.
   browser = init_browser()
   
-  # Set your url to the 'https://www.nytimes.com/search?query=california'
+  # Create a variable called "url" and set it to 'https://www.nytimes.com/search?query=california'
+    # Or select another state.
   url = "https://www.nytimes.com/search?query=california"
+
+  # Using browser, visit the url above.
   browser.visit(url)
 
+  # Declare a variable soup and parse through the html from browser using BeautifulSoup.
   html = browser.html
   soup = BeautifulSoup(html, "html.parser")
 
+  # Create an empty array called "state_news" that will be holding your news articles.
   state_news = []
 
-  # Selecting all news list elements to retrieve title and url.
+  # Loop through all news 'li' elements to retrieve title and url. 
+    # Then append the title and url as an object to state_news.
   for list_element in soup.select('li[class*="SearchResults-item"]'):
 
     title = list_element.find('h4').get_text()
@@ -37,4 +48,5 @@ def scrape_states():
 
     state_news.append(data)
 
+  # Return the state_news array
   return state_news
