9b3d5313288dc06c6bcfa74fd9c13bcd949ecc17
jimmytu <jimmytutron@gmail.com>
2018-12-04 21:19:16 -0800

    Leaflet.js mapping geoJSON of volcanoes

---
+++ Mapping/day-01/solved/logic.js
@@ -0,0 +1,72 @@
+// Store our API endpoint inside queryUrl
+var queryUrl = "https://data.humdata.org/dataset/a60ac839-920d-435a-bf7d-25855602699d/resource/7234d067-2d74-449a-9c61-22ae6d98d928/download/volcano.json";
+
+// Perform a GET request to the query URL
+d3.json(queryUrl, function(data) {
+  // Once we get a response, send the data.features object to the createFeatures function
+  createFeatures(data.features);
+});
+
+function createFeatures(volcanoData) {
+
+  // Define a function we want to run once for each feature in the features array
+  // Give each feature a popup describing the place and time of the volcano
+  function onEachFeature(feature, layer) {
+    layer.bindPopup("<h3>" + feature.properties.V_Name +
+      "</h3><hr><p>" + feature.properties.VEI_Holoce + "</p>");
+  }
+
+  // Create a GeoJSON layer containing the features array on the volcanoData object
+  // Run the onEachFeature function once for each piece of data in the array
+  var volcanos = L.geoJSON(volcanoData, {
+    onEachFeature: onEachFeature
+  });
+
+  // Sending our volcanos layer to the createMap function
+  createMap(volcanos);
+}
+
+function createMap(volcanos) {
+
+  // Define streetmap and darkmap layers
+  var streetmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "mapbox.streets",
+    accessToken: API_KEY
+  });
+
+  var darkmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "mapbox.dark",
+    accessToken: API_KEY
+  });
+
+  // Define a baseMaps object to hold our base layers
+  var baseMaps = {
+    "Street Map": streetmap,
+    "Dark Map": darkmap
+  };
+
+  // Create overlay object to hold our overlay layer
+  var overlayMaps = {
+    volcanos: volcanos
+  };
+
+  // Create our map, giving it the streetmap and volcanos layers to display on load
+  var myMap = L.map("map", {
+    center: [
+      37.09, -95.71
+    ],
+    zoom: 5,
+    layers: [streetmap, volcanos]
+  });
+
+  // Create a layer control
+  // Pass in our baseMaps and overlayMaps
+  // Add the layer control to the map
+  L.control.layers(baseMaps, overlayMaps, {
+    collapsed: false
+  }).addTo(myMap);
+}
